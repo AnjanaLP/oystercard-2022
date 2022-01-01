65af3239266f6e93d5ba7e9f3b75ce103e926b47
AnjanaLP <anjana.lpn@gmail.com>
2022-01-01 13:53:13 +0000

    User Story 2 In order to keep using public transport As a customer I want to add money to my card

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,10 +1,18 @@
 describe 'User Stories' do
+  let(:oystercard)    { Oystercard.new }
 
   # In order to use public transport
   # As a customer
   # I want money on my card
   it 'an oystercard initially has zero balance' do
-    oystercard = Oystercard.new
     expect(oystercard.balance).to eq 0
+  end
+
+  # In order to keep using public transport
+  # As a customer
+  # I want to add money to my card
+  it 'an oystercard can be topped up to increase the balance' do
+    oystercard.top_up(10)
+    expect(oystercard.balance).to eq(10)
   end
 end
--- spec/unit/oystercard_spec.rb
+++ spec/unit/oystercard_spec.rb
@@ -2,10 +2,16 @@ require 'oystercard'
 
 describe Oystercard do
   subject(:oystercard)    { described_class.new }
-  
+
   describe '#balance' do
     it 'is initially zero' do
       expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#top_up' do
+    it 'increases the balance by the given amount' do
+      expect { oystercard.top_up(10) }.to change { oystercard.balance }.by(10)
     end
   end
 end
